e5a0f5afadf242fb35b5f3bfd2d8610c3f8bc105
UditaVed <uditavishnoi18@gmail.com>
2023-02-09 11:40:41 +0530

    meta

---
+++ app/head.js
@@ -0,0 +1,9 @@
+const Head = () => (
+  <>
+    <title>Metaversus</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="https://gateway.pinata.cloud/ipfs/QmbGHtiHKUsf1qdL4B1ew43oQjMuc2tRuBquSHUMbA9FBB?_gl=1*erssq4*_ga*NDUxNzk1NjUzLjE2NzU2NTk5MTI.*_ga_5RMPXG14TE*MTY3NTY1OTkxMy4xLjEuMTY3NTY1OTk5MC41OC4wLjA." />
+  </>
+);
+
+export default Head;
